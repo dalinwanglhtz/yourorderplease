c9d3bc014b68ffb1b833695533fe2075c1ca31da
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-09 22:11:54 +1100

    add pattern matcher

--- src/com/codewardev/Order.java
+++ src/com/codewardev/Order.java
@@ -1,9 +1,19 @@
 package com.codewardev;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Order {
 
-	public static String order(String string) {
-		// TODO Auto-generated method stub
+	public static String order(String words) {
+		String[] word = words.split(" ");
+		String patternStr = "[0-9]";
+		Pattern pattern = Pattern.compile(patternStr);
+		for(String w: word) {
+			System.out.println(w);
+			Matcher matcher = pattern.matcher(w);
+			if(matcher.find()) System.out.println("Find: "+w.charAt(matcher.start()));
+		}
 		return null;
 	}
 
