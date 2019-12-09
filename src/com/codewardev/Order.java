3efcc6d2104ea7e835694cdaf29dd68466e8509c
Dale Wang <dalinwanglhtz@gmail.com>
2019-12-09 22:33:58 +1100

    resolved

--- src/com/codewardev/Order.java
+++ src/com/codewardev/Order.java
@@ -1,20 +1,31 @@
 package com.codewardev;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Order {
 
 	public static String order(String words) {
+		
+		if(words.isEmpty()) return "";
+		
 		String[] word = words.split(" ");
 		String patternStr = "[0-9]";
 		Pattern pattern = Pattern.compile(patternStr);
-		for(String w: word) {
-			System.out.println(w);
-			Matcher matcher = pattern.matcher(w);
-			if(matcher.find()) System.out.println("Find: "+w.charAt(matcher.start()));
+		for(int i=0; i<word.length; i++) {
+			Matcher matcher = pattern.matcher(word[i]);
+			int digit=-1;
+			if(matcher.find()) digit = Character.getNumericValue(word[i].charAt(matcher.start()));
+			word[i] = digit + word[i];
 		}
-		return null;
+		
+		return Arrays
+				.stream(word)
+				.sorted()
+				.map(x->x.substring(1))
+				.collect(Collectors.joining(" "));
 	}
 
 }
